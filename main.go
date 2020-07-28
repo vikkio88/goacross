ee9cb0730e3a20810bf95d0fd289ca12b32dfe9c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-07-28 20:08:53 +0100

    testing image loader

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"image"
+	"os"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
-func run() {
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
 
+func run() {
 	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
+		Title:  "Go Across",
 		Bounds: pixel.R(0, 0, 1024, 768),
 		VSync:  true,
 	}
@@ -19,7 +34,15 @@ func run() {
 		panic(err)
 	}
 
+	pic, err := loadPicture("assets/gopher.png")
+	if err != nil {
+		panic(err)
+	}
+
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+
 	win.Clear(colornames.Skyblue)
+	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
 
 	for !win.Closed() {
 		if win.JustPressed(pixelgl.KeyEscape) || win.JustPressed(pixelgl.KeyQ) {
